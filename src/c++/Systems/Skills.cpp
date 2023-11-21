d3e2cba42f77ad7031accaf5ef6d35cfac384bec
Flenarn <smallmustard@gmail.com>
2023-11-21 21:49:39 +0100

    Further progress on the Skills logic, all custom forms hooked up.

--- src/c++/Systems/Skills.cpp
+++ src/c++/Systems/Skills.cpp
@@ -1,8 +1,8 @@
 #include "Skills.h"
 #include "Shared/SharedFunctions.h"
 
-REL::Relocation<uintptr_t> ActorValueDerivedVtbl{ REL::ID(337946) };
-REL::Relocation<uintptr_t> ActorValueCalcVtbl{ REL::ID(510751) };
+REL::Relocation<uintptr_t> ActorValueDerivedVtbl	{ REL::ID(337946) };
+REL::Relocation<uintptr_t> ActorValueCalcVtbl		{ REL::ID(510751) };
 
 class ActorValueInfo;
 
@@ -49,6 +49,8 @@ namespace Skills
 
 	std::unordered_multimap<RE::ActorValueInfo*, RE::ActorValueInfo*> skillsLinkMap;
 	std::unordered_multimap<std::string, RE::ActorValueInfo*> strSkillMap;
+
+	AVVector CascadiaSkillsList;
 
 	// Returns ActorValueInfo based on Skill Name.
 	RE::ActorValueInfo* GetSkillByName(std::string mySkill)
@@ -108,6 +110,25 @@ namespace Skills
 		{
 			dependentAV->dependentActorValues[dependentAV->numDependentActorValues++] = myAV;
 		}
+	}
+
+	void RegisterDerivedAV(RE::ActorValueInfo* myAV, float(*f)(RE::ActorValueOwner*, RE::ActorValueInfo&))
+	{
+		// TODO
+	}
+
+	void RegisterLinkedAV(RE::ActorValueInfo* myAV, float(*CalcFunction)(RE::ActorValueOwner*, RE::ActorValueInfo&), RE::ActorValueInfo* av1, RE::ActorValueInfo* av2)
+	{
+		RegisterDerivedAV(myAV, CalcFunction);
+		AddDependentAV(myAV, av1);
+		AddDependentAV(myAV, av2);
+		skillsLinkMap.emplace(myAV, av1);
+	}
+
+	void RegisterCalc(RE::ActorValueInfo* myAV, void(*f)(RE::Actor*, RE::ActorValueInfo&, float, float, RE::Actor*))
+	{
+		//myAV->VTABLE = reinterpret_cast<void*>(REL::Relocation(ActorValueCalcVtbl).get());
+		myAV->derivationFunction = f;
 	}
 
 	float CalculateSkillOffset(RE::ActorValueOwner* myAVOwner, RE::ActorValueInfo& myAV)
@@ -173,4 +194,56 @@ void ModPlayerBaseAVValue(RE::ActorValueInfo* myAV, float fModAmount)
 void SetPlayerBaseAVValue(RE::ActorValueInfo* myAV, float fSetAmount)
 {
 	SetBaseAVValue(GetPlayerCharacter(), myAV, fSetAmount);
+}
+
+bool DefineSkillsFormsFromGame()
+{
+	// Vanilla Actor Values
+	VanillaActorValues.Strength		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(706, "Fallout4.esm")); //HEX: 2C2
+	VanillaActorValues.Perception	= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(707, "Fallout4.esm")); //HEX: 2C3
+	VanillaActorValues.Endurance	= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(708, "Fallout4.esm")); //HEX: 2C4
+	VanillaActorValues.Charisma		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(709, "Fallout4.esm")); //HEX: 2C5
+	VanillaActorValues.Intelligence	= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(710, "Fallout4.esm")); //HEX: 2C6
+	VanillaActorValues.Agility		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(711, "Fallout4.esm")); //HEX: 2C7
+	VanillaActorValues.Luck			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(712, "Fallout4.esm")); //HEX: 2C8
+
+	// Cascadia Actor Values
+	CascadiaActorValues.Barter			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996845, "FalloutCascadia.esm")); //HEX: F35ED
+	CascadiaActorValues.EnergyWeapons	= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996846, "FalloutCascadia.esm")); //HEX: F35EE
+	CascadiaActorValues.Explosives		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996847, "FalloutCascadia.esm")); //HEX: F35EF
+	CascadiaActorValues.Guns			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996848, "FalloutCascadia.esm")); //HEX: F35F0
+	CascadiaActorValues.Lockpick		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996849, "FalloutCascadia.esm")); //HEX: F35F1
+	CascadiaActorValues.Medicine		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996850, "FalloutCascadia.esm")); //HEX: F35F2
+	CascadiaActorValues.MeleeWeapons	= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996851, "FalloutCascadia.esm")); //HEX: F35F3
+	CascadiaActorValues.Repair			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996852, "FalloutCascadia.esm")); //HEX: F35F4 
+	CascadiaActorValues.Science			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996853, "FalloutCascadia.esm")); //HEX: F35F5
+	CascadiaActorValues.Sneak			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996854, "FalloutCascadia.esm")); //HEX: F35F6
+	CascadiaActorValues.Speech			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996855, "FalloutCascadia.esm")); //HEX: F35F7
+	CascadiaActorValues.Survival		= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996856, "FalloutCascadia.esm")); //HEX: F35F8 
+	CascadiaActorValues.Unarmed			= reinterpret_cast<RE::ActorValueInfo*>(RE::TESDataHandler().LookupForm(996857, "FalloutCascadia.esm")); //HEX: F35F9
+
+	// Cascadia Perks
+	CascadiaPerks.WeaponTypeEnergyWeaponsPerk	= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626494, "FalloutCascadia.esm")); //HEX: 21F9DFE
+	CascadiaPerks.WeaponTypeExplosivesPerk		= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626495, "FalloutCascadia.esm")); //HEX: 21F9DFF
+	CascadiaPerks.WeaponTypeMeleeWeaponsPerk	= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626496, "FalloutCascadia.esm")); //HEX: 21F9E00
+	CascadiaPerks.WeaponTypeGunsPerk			= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626497, "FalloutCascadia.esm")); //HEX: 21F9E01
+	CascadiaPerks.WeaponTypeUnarmedPerk			= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626498, "FalloutCascadia.esm")); //HEX: 21F9E02
+	CascadiaPerks.WeaponConditionHandlerPerk	= reinterpret_cast<RE::BGSPerk*>(RE::TESDataHandler().LookupForm(35626499, "FalloutCascadia.esm")); //HEX: 21F9E04
+
+	// Cascadia Skills List
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Barter);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.EnergyWeapons);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Explosives);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Guns);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Lockpick);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Medicine);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.MeleeWeapons);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Repair);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Science);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Sneak);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Speech);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Survival);
+	Skills::CascadiaSkillsList.emplace_back(CascadiaActorValues.Unarmed);
+
+	return true;
 }
--- src/c++/Systems/Skills.h
+++ src/c++/Systems/Skills.h
@@ -2,6 +2,9 @@
 
 #include "GameForms.h"
 
+typedef std::vector<RE::ActorValueInfo*> AVVector;
+
+
 enum SPECIALFormIDs 
 {
 	StrengthID = 706,
@@ -22,6 +25,8 @@ namespace Skills
 
 	RE::ActorValueInfo* GetDependantAV(RE::ActorValueInfo* myAV);
 	void AddDependentAV(RE::ActorValueInfo* myAV, RE::ActorValueInfo* dependantAV);
+	void RegisterDerivedAV(RE::ActorValueInfo* myAV, float(*f)(RE::ActorValueOwner*, RE::ActorValueInfo&));
+	void RegisterLinkedAV(RE::ActorValueInfo* myAV, float(*CalcFunction)(RE::ActorValueOwner*, RE::ActorValueInfo&), RE::ActorValueInfo* av1, RE::ActorValueInfo* av2);
 
 	float CalculateSkillOffset(RE::ActorValueOwner* myAVOwner, RE::ActorValueInfo& myAV);
 }
@@ -35,3 +40,5 @@ float GetPlayerAVValue(RE::ActorValueInfo* myAV);
 float GetPlayerBaseAVValue(RE::ActorValueInfo* myAV);
 void ModPlayerBaseAVValue(RE::ActorValueInfo* myAV, float fModAmount);
 void SetPlayerBaseAVValue(RE::ActorValueInfo* myAV, float fSetAmount);
+
+bool DefineSkillsFormsFromGame();

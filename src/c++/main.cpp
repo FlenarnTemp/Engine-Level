3b5b71df5075c626e4a377b540316331d29425a0
Flenarn <smallmustard@gmail.com>
2023-11-26 13:33:41 +0100

    Add more prints for MessageInterface types, fix formatting.

--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -15,8 +15,8 @@ namespace
 
 		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 		auto lvl = *Settings::General::EnableDebugLogging
-		               ? spdlog::level::trace
-		               : spdlog::level::info;
+			? spdlog::level::trace
+			: spdlog::level::info;
 
 		log->set_level(lvl);
 		log->flush_on(lvl);
@@ -36,25 +36,57 @@ namespace
 
 		switch (a_msg->type)
 		{
-			case F4SE::MessagingInterface::kGameDataReady:
-				logger::info(FMT_STRING("{:s} - kGameDataReady"), Version::PROJECT);
-				break;
+		case F4SE::MessagingInterface::kGameDataReady:
+			logger::info(FMT_STRING("{:s} - kGameDataReady"), Version::PROJECT);
+			break;
 
-			case F4SE::MessagingInterface::kGameLoaded:
-				logger::info(FMT_STRING("{:s} - kGameLoaded"), Version::PROJECT);
-				break;
+		case F4SE::MessagingInterface::kGameLoaded:
+			logger::info(FMT_STRING("{:s} - kGameLoaded"), Version::PROJECT);
+			break;
 
-			case F4SE::MessagingInterface::kPostLoadGame:
-				logger::info(FMT_STRING("{:s} - kPostLoadGame"), Version::PROJECT);
-				break;
+		case F4SE::MessagingInterface::kPostLoad:
+			logger::info(FMT_STRING("{:s} - kPostLoad"), Version::PROJECT);
+			break;
 
-			default:
-				break;
+		case F4SE::MessagingInterface::kPostLoadGame:
+			logger::info(FMT_STRING("{:s} - kPostLoadGame"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kPostPostLoad:
+			logger::info(FMT_STRING("{:s} - kPostPostLoad"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kPostSaveGame:
+			logger::info(FMT_STRING("{:s} - kPostSaveGame"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kDeleteGame:
+			logger::info(FMT_STRING("{:s} - kDeleteGame"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kNewGame:
+			logger::info(FMT_STRING("{:s} - kNewGame"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kInputLoaded:
+			logger::info(FMT_STRING("{:s} - kInputLoaded"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kPreLoadGame:
+			logger::info(FMT_STRING("{:s} - kPreLoadGame"), Version::PROJECT);
+			break;
+
+		case F4SE::MessagingInterface::kPreSaveGame:
+			logger::info(FMT_STRING("{:s} - kPreSaveGame"), Version::PROJECT);
+			break;
+
+		default:
+			break;
 		}
 	}
 }
 
-extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_F4SE, F4SE::PluginInfo* a_info)
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface * a_F4SE, F4SE::PluginInfo * a_info)
 {
 	a_info->infoVersion = F4SE::PluginInfo::kVersion;
 	a_info->name = Version::PROJECT.data();
@@ -73,7 +105,7 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a
 	return true;
 }
 
-extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_F4SE)
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface * a_F4SE)
 {
 	InitializeLog();
 
@@ -86,7 +118,7 @@ extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_F
 	const auto messaging = F4SE::GetMessagingInterface();
 	if (!messaging || !messaging->RegisterListener(MessageHandler))
 
-	ObScript::Install();
+		ObScript::Install();
 
 	logger::info(FMT_STRING("{:s} finished loading."), Version::PROJECT);
 

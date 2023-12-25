ba299a2bd2421ffafa47b7479c5432ae0b6939f9
Flenarn <smallmustard@gmail.com>
2023-12-25 14:53:46 +0100

    chore: Nuke most of AmmoSwitch.cpp.

--- src/c++/Systems/AmmoSwitch.cpp
+++ src/c++/Systems/AmmoSwitch.cpp
@@ -4,17 +4,4 @@ namespace AmmoSwitch
 {
 	AmmoSwitchValues_Struct AmmoSwitchValues;
 
-	// Input Handling
-
-	class InputEventWatcher : public RE::BSTEventSink<RE::InputEvent>
-	{
-	public:
-		virtual RE::BSEventNotifyControl ProcessEvent(const RE::InputEvent& evn, RE::BSTEventSource<RE::InputEvent>*)
-		{
-			// code code code
-
-			return RE::BSEventNotifyControl::kContinue;
-		}
-	};
-
 }

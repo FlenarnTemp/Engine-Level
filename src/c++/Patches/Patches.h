39ff0fd2192301b9808fa886fb6d478838084d6e
Flenarn <smallmustard@gmail.com>
2024-01-03 22:50:41 +0100

    feat: Small shit

--- src/c++/Patches/Patches.h
+++ src/c++/Patches/Patches.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "Patches/LoadEditorIDs.h"
+#include "Patches/OnStoryCraftItem.h"
 
 namespace Patches
 {
 	void Install()
 	{
 		LoadEditorIDs::Install();
+		OnStoryCraftItem::Install();
 	}
 }

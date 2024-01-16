370cd77a4f4bded2c08d3747d23a0f56b82e6410
Flenarn <smallmustard@gmail.com>
2024-01-16 21:34:31 +0100

    feat: More event logic.

--- src/c++/Events/TESInitScriptEvent.h
+++ src/c++/Events/TESInitScriptEvent.h
@@ -3,6 +3,8 @@
 #include "RE/Bethesda/BSTEvent.h"
 #include "RE/Bethesda/Events.h"
 
+#include "Systems/ItemDegradation.h"
+
 namespace RE
 {
 	namespace Cascadia
@@ -11,20 +13,28 @@ namespace RE
 		{
 			virtual BSEventNotifyControl ProcessEvent(const TESInitScriptEvent& event, BSTEventSource<TESInitScriptEvent>*) override
 			{
-				if (TESObjectREFR* reference = event.hObjectInitialized) {
-					switch (reference->formType.underlying())
+				if (event.hObjectInitialized) {
+
+					switch (event.hObjectInitialized->data.objectReference->formType.underlying())
 					{
 					case static_cast<std::uint32_t>(ENUM_FORM_ID::kCONT):
 						logger::info("InitializeInventoryCondition");
 						break;
 
+					case static_cast<std::uint32_t>(ENUM_FORM_ID::kARMO):
+						logger::info("InitializeArmorCondition");
+						break;
+
+					case static_cast<std::uint32_t>(ENUM_FORM_ID::kWEAP):
+						InitializeWeaponCondition(event.hObjectInitialized);
+						logger::info("InitializeWeaponCondition");
+						break;
+
 					default:
+						logger::debug(FMT_STRING("Form type: {:s}"), std::to_string(event.hObjectInitialized->data.objectReference->formType.underlying()));
 						break;
 					}
 				}
-
-				logger::info("TEST");
-
 				return BSEventNotifyControl::kContinue;
 			}
 		};
